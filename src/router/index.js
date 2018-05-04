19a7ab2333d4b898e6f0fc82e9dcc6cdde358e91
namehu <13661958468@163.com>
2018-05-04 22:54:26 +0800

    代码提交

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
 import Login from '@/components/Login';
 import Register from '@/components/Register';
 
@@ -8,10 +7,6 @@ Vue.use(Router);
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      component: HelloWorld,
-    },
     {
       path: '/login',
       component: Login,
